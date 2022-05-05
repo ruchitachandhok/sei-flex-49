7a797e0b3c11a174ae04b1e2dd7559922923c4aa
alex ghose <alex.ghose@generalassemb.ly>
2022-05-04 22:44:00 -0400

    fix typos

--- w10/d2/consuming-3rd-party-apis/instructor-2022/github-users/routes/index.js
+++ w10/d2/consuming-3rd-party-apis/instructor-2022/github-users/routes/index.js
@@ -7,24 +7,22 @@ const axios = require('axios').default;
 router.get('/', async function(req, res, next) {
 
   const username = req.query.username1; // handle incoming query parameters
-
   let token = process.env.GITHUB_TOKEN // access one of our env variables
 
-  // right-hand-side sends a message to github API
-  // left-hand-side stores the response
   try {
-
     let obj = {
       headers: {
         'User-Agent': 'alexanderghose',
         Authorization: `token ${token}`,
       }
     }
+
+    // right-hand-side sends a message to github API, left-hand-side stores the response
     const response = await axios.get(`https://api.github.com/users/${username}`, obj)
     console.log("we talked to github and GH said", response.data)
     res.render('index.ejs', { title: 'Express', data: response.data, });
 
-  } catch(err) { // if there's an error the catch block triggers
+  } catch(err) { // if there's an error, the catch block triggers
     
     console.log("failed")
     res.render('index.ejs', { title: 'Error'})
